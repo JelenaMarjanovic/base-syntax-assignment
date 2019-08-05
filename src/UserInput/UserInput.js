47117284cd0b6206050d48b3904fd36cd830f498
Jelena Marjanović <jelena.marjanovic.13@gmail.com>
2019-08-05 03:45:07 +0200

    Add styling

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const UserInput = props => {
+  const style = {
+    padding: '8px',
+    margin: '16px',
+    border: '3px solid blueviolet'
+  };
+
   return (
-    <input type="text" value={props.currentUsername} onChange={props.changed} />
+    <input
+      type="text"
+      style={style}
+      value={props.currentUsername}
+      onChange={props.changed}
+    />
   );
 };
 
