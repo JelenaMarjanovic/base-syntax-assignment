eecea182c789c38a30a7856dae830d37551fe6f1
Jelena Marjanović <jelena.marjanovic.13@gmail.com>
2019-08-05 03:13:15 +0200

    Create UserOutput

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserOutput = () => {
+  return (
+    <div>
+      <p>Username:</p>
+      <p>(Will be overwritten.)</p>
+    </div>
+  );
+};
+
+export default UserOutput;
